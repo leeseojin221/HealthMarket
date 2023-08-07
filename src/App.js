b2a805345cf1c0cf4e0c8884697c5e50180a419b
leeseojin221 <134032937+leeseojin221@users.noreply.github.com>
2023-08-07 17:48:18 +0900

    Revert "Feat:헤더 푸터 작업"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
 import './App.css';
-import Router from './shared/Router';
-
-const queryClient = new QueryClient()
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Router />
-    </QueryClientProvider>
-  )
+  return <div className="App"></div>;
 }
 
 export default App;
