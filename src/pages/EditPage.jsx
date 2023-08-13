43fc90721c1c3a51d78effc30f77296a901e9ef9
KimEijin <jianchidaodi1029@naver.com>
2023-08-14 03:22:44 +0900

    1

--- src/pages/EditPage.jsx
+++ src/pages/EditPage.jsx
@@ -199,17 +199,38 @@ const StImgDiv = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  /* padding: 20px; */
+  padding: 20px;
   margin: 20px;
   border-radius: 5px;
-  /* justify-content: center; */
+  justify-content: center;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
   width: 500px;
   height: 500px;
 
+  .file-input-label {
+    position: absolute;
+    top: 90%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    /* background-color: #F8F8F8; */
+    padding: 10px 20px;
+    /* border: 1px solid #ccc;
+    border-radius: 5px; */
+    cursor: pointer;
+  }
+  .file-input {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    cursor: pointer;
+  }
+
   img {
-    /* max-width: 100%;
-    max-height: 100%; */
+    max-width: 100%;
+    max-height: 100%;
     width: 100%;
     height: 100%;
     object-fit: contain;
